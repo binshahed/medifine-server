830873d85cba2282d7246e16a3aab36d29293968
Shahed <mdbinshahed5@gmail.com>
2021-12-09 03:58:53 +0600

    add put mehod

--- index.js
+++ index.js
@@ -21,12 +21,50 @@ async function run () {
     await client.connect()
     const database = client.db('medicare')
     const appointmentsCollection = database.collection('appointments')
+    const usersCollection = database.collection('users')
 
+    // Get Appointment
+    app.get('/appoints', async (req, res) => {
+      const email = req.query.email
+      const date = new Date(req.query.date).toLocaleDateString()
+      console.log(date)
+      const query = { email: email, date: date }
+      const cursor = appointmentsCollection.find(query)
+      const appointments = await cursor.toArray()
+      res.json(appointments)
+    })
     // Post Appointment
     app.post('/appoints', async (req, res) => {
       const appointment = req.body
       console.log(appointment)
       const result = await appointmentsCollection.insertOne(appointment)
+      res.json(result)
+    })
+
+    //POST user
+    app.post('/users', async (req, res) => {
+      const users = req.body
+      console.log(users)
+      const result = await usersCollection.insertOne(users)
+      res.json(result)
+    })
+    // PUT user
+    app.put('/users', async (req, res) => {
+      const user = req.body
+      const filter = { email: user.email }
+      const options = { upsert: true }
+      const updateDoc = { $set: user }
+      const result = await usersCollection.updateOne(filter, updateDoc, options)
+      res.json(result)
+    })
+    // PUT ‍admin
+    app.put('/users/admin', async (req, res) => {
+      const user = req.body
+      const filter = { email: user.email }
+      const options = { upsert: true }
+      const updateDoc = { $set: { role: 'admin' } }
+      const result = await usersCollection.updateOne(filter, updateDoc, options)
+      console.log(result)
       res.json(result)
     })
   } finally {
